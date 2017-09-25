e2101a82227ba364f2e1871bd16cd31bef6316fe
root <root@deploy001.bjb.monitor>
2017-09-25 18:42:57 +0800

    second commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def Index(request):
+	test_dict = {'1': 'one', '2': 'two'}
+	return render(request, 'index.html', {'test_dict': test_dict})
